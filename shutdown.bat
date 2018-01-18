8083fcd8504ed7b40b4836a962caee124ccdd91d
Ricky Chen <Ricky.chen98@hotmail.com>
2018-01-18 09:44:29 -0500

    remark for seconds -> hours

--- shutdown.bat
+++ shutdown.bat
@@ -1,5 +1,6 @@
 @echo off
 color 0A
-echo Type in the time until shutdown (in seconds):
+rem 3600 seconds in an hour
+echo Type in the time until shutdown (in seconds, 3600 seconds in an hour):
 set /p time=
 shutdown -s -f -t %time%
