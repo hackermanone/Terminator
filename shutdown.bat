9f4219a9915970dd6eab01424118bbc4003e45ef
Ricky Chen <Ricky.chen98@hotmail.com>
2018-01-19 15:14:05 -0500

    Skeleton to allow the use of hours, minutes, and seconds

--- shutdown.bat
+++ shutdown.bat
@@ -3,4 +3,8 @@ color 0A
 rem 3600 seconds in an hour
 echo Type in the time until shutdown (in seconds, 3600 seconds in an hour):
 set /p time=
+rem TODO allow for the use of s for seconds, m for minutes and h for hours
+rem remove spaces
+set time=%time: =%
+rem extract last 4 cahracters from string, str=%str:~-4% -- Test
 shutdown -s -f -t %time%
