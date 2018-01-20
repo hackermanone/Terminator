538662011e6260ff018e55c9e305517ee87ac3df
Ricky Chen <Ricky.chen98@hotmail.com>
2018-01-20 13:07:01 -0500

    Created function for hours

--- shutdown.bat
+++ shutdown.bat
@@ -6,5 +6,13 @@ set /p time=
 rem TODO allow for the use of s for seconds, m for minutes and h for hours
 rem remove spaces
 set time=%time: =%
-rem extract last 4 cahracters from string, str=%str:~-4% -- Test
-shutdown -s -f -t %time%
+rem check suffix after removing white spaces
+set suffix=%time:~-1%
+if %suffix% == "h" (
+goto hours
+)
+rem extract last 4 characters from string, str=%str:~-4% -- Test
+:hours
+rem change time to seconds, hours*60*60 = seconds
+set /a seconds=%time%*3600
+shutdown -s -f -t %seconds%
