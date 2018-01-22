eb9ec39c5c94695f81b86449bb4f24f3c30a2e7b
Ricky Chen <Ricky.chen98@hotmail.com>
2018-01-22 16:13:30 -0500

    Deleted shutdown.bat and hours.bat, implemented both in timedShutdown.bat

--- hours.bat
+++
@@ -1,5 +0,0 @@
-@ECHO off
-rem change time to seconds, hours*60*60 = seconds
-set /a seconds=%time%*3600
-shutdown -s -f -t %seconds%
-exit
---
+++ timedShutdown.bat
@@ -0,0 +1,20 @@
+@echo off
+color 0A
+title shutdown
+
+:: 3600 seconds in an hour
+echo Type in the time until shutdown:
+set /p time=
+echo Type hours, minutes, or seconds (h, m, or s)?
+set /p suffix=
+:: check suffix
+if "%suffix%" == "h" goto hours
+if "%suffix%" == "s" goto seconds
+
+:hours
+set /a seconds=%time%*3600
+shutdown -s -f -t %seconds%
+
+:seconds
+shutdown -s -f -t %time%
+pause
